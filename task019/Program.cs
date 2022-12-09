e4802a9a0e7de9e5ec75235d0169b1701bb44e4e
Artemiev Dima <adbart1985@gmail.com>
2022-12-09 19:14:18 +0300

    solved

--- task019/Program.cs
+++ task019/Program.cs
@@ -4,3 +4,23 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+//Решение №1
+
+Console.Write("Input number ");
+string number = Console.ReadLine() ??"";
+
+void CheckingNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"Your number: {number} - palindrom.");
+    }
+    else Console.WriteLine($"Your number: {number} - not palindrom.");
+}
+
+if (number!.Length == 5)
+{
+    CheckingNumber(number);
+}
+else Console.WriteLine("Input right number");
