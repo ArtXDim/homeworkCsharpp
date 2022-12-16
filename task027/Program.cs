8ce29be13c7bd86f144cfecba93a82869d0e97c2
Artemiev Dima <adbart1985@gmail.com>
2022-12-16 18:56:12 +0300

    solved

--- task027/Program.cs
+++ task027/Program.cs
@@ -3,3 +3,25 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.WriteLine("Task #27");
+Console.WriteLine("Input number N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumLenNumber(int n)
+{
+    int counter = Convert.ToInt32(n);
+    int result = 0;
+    int advance = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        advance = n - n % 10;
+        result = result + (n - advance);
+        n = n / 10;
+    }
+    return result;
+}
+
+int Sum = SumLenNumber(n);
+Console.WriteLine($"a^(b) = {SumLenNumber(n)}");
