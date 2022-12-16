9003f740580455173b8c2ad2a1ce9a6ca90294d4
Artemiev Dima <adbart1985@gmail.com>
2022-12-16 19:13:11 +0300

    solved

--- task029/Program.cs
+++ task029/Program.cs
@@ -3,3 +3,21 @@
 //  и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Task #29");
+
+int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0,11);
+    Console.Write($" {Method(i)} ");
+}
+
+Console.Write("]");
+
+int Method(int n)
+{
+    return numbers[n];
+}
