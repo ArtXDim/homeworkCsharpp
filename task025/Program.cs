08405660fb7610f809957c46ccdc9c8648f2b6c9
Artemiev Dima <adbart1985@gmail.com>
2022-12-16 06:44:56 +0300

    New tasks

--- task025/Program.cs
+++ task025/Program.cs
@@ -3,3 +3,37 @@
 // и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.WriteLine("Task #25");
+
+int GetNumber(string message)
+{
+    //bool isCorrect = false; // bool - принимает булиевая переменная true false. 
+    while (true)
+    {
+        Console.WriteLine(message);
+        string number = Console.ReadLine() ?? "";
+        if (int.TryParse(number, out int result))
+        {
+            return result;
+        }
+        else
+        {
+            Console.WriteLine("Wrong input. Please input correct number");
+        }
+    }
+}
+
+Get
+
+Console.Write("Input number a=");
+int a = Convert.ToInt32(Console.ReadLine() ??"");
+Console.Write("Input number b=");
+int b = Convert.ToInt32(Console.ReadLine() ??"");
+int result = 0;
+
+Console.WriteLine($"{Math.Pow(a,b)}");
+
+
+
+
---
+++ trash/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+int GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            return result; //break; //isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Wrong input. Please input number");
+        }
+    }
+
+}
+
+int  GetMultiply(int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result = (int)Math.Pow(i, 3);
+    }
+    return(result);
+}
+
+
+int number = GetNumber("Input value N ");
+Console.WriteLine($"{GetMultiply(number)} ");
+
+
+int number = ReadInt("Input number: ");
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i * i * i}, ");
+}
+
+// Метод
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+*/
